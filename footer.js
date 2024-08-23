6e88a54ba28ee7e39015f7b6c533ccfe86101bda
Hrishikesh Kalita <95520030+HrishiD89@users.noreply.github.com>
2024-08-24 00:01:50 +0530

    first commit

---
+++ footer.js
@@ -0,0 +1,28 @@
+const Company  = [
+"About",
+"Products",
+"Pricing",
+"Referral programme",
+"Careers",
+"Zerodha.tech",
+"Press & media",
+"Zerodha Cares (CSR)",
+];
+
+const Support = [
+    "Contact us",
+    "Support portal",
+    "Z-Connect blog",
+    "List of charges",
+    "Downloads & resources",
+    "Videos",
+    "Market overview",
+    "How to file a complaint?",
+    "Status of your complaints",
+]
+
+const Account = [
+    "Open and account",
+    "Fund transfer",
+
+]
